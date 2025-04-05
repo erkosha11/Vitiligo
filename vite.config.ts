b17ba8f3dda6195cc70576e8f415bad95a96e9b3
Erkosha <erkebulan003@yandex.ru>
2025-04-05 17:27:15 +0500

    init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      app: path.resolve(__dirname, 'src/app'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      entities: path.resolve(__dirname, 'src/entities'),
+      features: path.resolve(__dirname, 'src/features'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      shared: path.resolve(__dirname, 'src/shared'),
+      widgets: path.resolve(__dirname, 'src/widgets')
+    }
+  }
+})
